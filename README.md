eaab4e75b09190f185fc1c66774905a138612113
Kris Szafranski <kdszafranski@gmail.com>
2016-05-19 13:06:18 -0500

    updated to correct folder structure as other branches.

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# iota-angular-basics
+# prime-angular-basics
 Lectures covering Angular. See Branches for demo code for each lecture/topic.
 
 npm install to get started.
 
 ##Branches
 * master: Lecture #1. Includes basic Angular module/controller with ng-repeat directive, $scope, functions, and local variables.
-* custom-directive-http: Lecture #2. Includes custom directive with HTML partial, $http usage for POST/GET.
+* custom-directives-http: Lecture #2. Includes custom directive with HTML partial, $http usage for POST/GET.
 * routing: Angular routing using ngRoute and app.config
+* factories: Angular Factories implemented. Makes use of $http promise/deferred objects to handle async issues
--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Serve back static files
+app.use(express.static(path.join(__dirname, './public')));
+
+// Routes
 app.get('/data', function(req, res) {
     res.send({message: 'hello'});
 });
@@ -13,12 +18,10 @@ app.post('/data/:number', function(req, res) {
     res.send(req.params.number);
 });
 
-// Serve back static files
-app.use(express.static('public'));
-app.use(express.static('public/views'));
-app.use(express.static('public/scripts'));
-app.use(express.static('public/styles'));
-app.use(express.static('public/vendors'));
+// Handle index file separately
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/views/index.html'));
+})
 
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function() {
