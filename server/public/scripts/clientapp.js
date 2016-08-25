50750ca492cfe463a8cd28e46902eee2f8ad1fe6
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-08-24 19:12:40 -0500

    pretty much done

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -4,11 +4,19 @@ myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/home', {
       templateUrl: '/views/home.html',
-      controller: "HomeController"
+      //controller: "HomeController"
     })
-    .when('/orders', {
-      templateUrl: '/views/orders.html',
-      controller: "OrdersController"
+    .when('/mystic', {
+      templateUrl: '/views/mystic.html',
+      //controller: "OrdersController"
+    })
+    .when('/valor', {
+      templateUrl: '/views/valor.html',
+      //controller: "OrdersController"
+    })
+    .when('/instinct', {
+      templateUrl: '/views/instinct.html',
+      //controller: "OrdersController"
     })
     .otherwise({
       redirectTo: 'home'
